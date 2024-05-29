55d1afa909429c18230f2f663b39ce8d7c9c8298
Rzelek <55500426+Rzelekk@users.noreply.github.com>
2024-05-29 15:51:26 +0200

    add clientController

---
+++ WebApplication1/WebApplication1/Controllers/ClientsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ClientsController : ControllerBase
+{
+    private readonly ApbdContext _context;
+
+    public ClientsController(ApbdContext context)
+    {
+        _context = context;
+    }
+
+    [HttpDelete]
+    [Route("{idClient}")]
+    public async Task<IActionResult> Delete(int idClient)
+    {
+        var totalTrips = await _context.ClientTrips.CountAsync(e => e.IdClient == idClient);
+        if (totalTrips > 0)
+        {
+            return Conflict($"This client id: {idClient} has booked trips");
+        }
+        
+        var clientToDelate = await _context.Clients.FindAsync(idClient);
+
+        if (clientToDelate != null)
+        {
+            _context.Clients.Remove(clientToDelate);
+            await _context.SaveChangesAsync();
+        }
+        else
+        {
+            return NotFound($"Client with given id: {idClient} does not exist");
+        }
+
+        return NoContent();
+    }
+}
