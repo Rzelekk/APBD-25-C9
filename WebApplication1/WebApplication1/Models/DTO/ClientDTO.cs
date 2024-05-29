e181e8cfdb20bb56588317a154cb2b6305d64577
Rzelek <55500426+Rzelekk@users.noreply.github.com>
2024-05-29 15:51:16 +0200

    add client dto

---
+++ WebApplication1/WebApplication1/Models/DTO/ClientDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models.DTO;
+
+public class ClientDTO
+{
+    
+    [Required]
+    [MaxLength(120)]
+    public string FirstName { get; set; } = null!;
+    [Required]
+    [MaxLength(120)]
+    public string LastName { get; set; } = null!;
+    [Required]
+    [MaxLength(120)]
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+    [Required]
+    [MaxLength(12)]
+    [Phone]
+    public string Telephone { get; set; } = null!;
+    [Required]
+    [StringLength(11, MinimumLength = 11)]
+    public string Pesel { get; set; } = null!;
+    [DataType(DataType.Currency)]
+    public DateTime? PaymentDate { get; set; }
+}
